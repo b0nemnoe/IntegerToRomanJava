78e085c65df6bb0455462d0e4d245fdbd8a8db6e
b0nemnoe <b0nemnoe@vasvari.org>
2024-11-28 23:03:22 +0100

    Kész

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Adjon meg egy számot:");
+        int inputNumber = scanner.nextInt();
+
+        String output = IntegerToRoman(inputNumber);
+        System.out.println("A megadott szám ("+ inputNumber + ") római alakja: " + output);
     }
 
-    public
+    public static String IntegerToRoman(int number)
+    {
+        StringBuilder roman = new StringBuilder();
+        String[] romanNumbers = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] numbers = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+        for (int i = 0; number > 0; i++) {
+            while (number >= numbers[i])
+            {
+                roman.append(romanNumbers[i]);
+                number -= numbers[i];
+            }
+        }
+        return roman.toString();
+    }
 
 }
